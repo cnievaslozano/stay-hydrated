4959586af262aa0a02653c3914af4f8d1d701dcb
cnievaslozano <cnievaslozano@gmail.com>
2024-07-19 19:48:21 +0200

    flash border color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Bottle } from "./Bottle";
 import { Header } from "./Header";
 
-export const Layout = ({ children }) => {
+export const Layout = () => {
+  const [borderColor, setBorderColor] = useState("transparent");
+
+  const flashBorderColor = (whatColor) => {
+    if (whatColor) {
+      setBorderColor("rgba(0, 0, 255, 0.3)");
+    } else {
+      setBorderColor("rgba(255, 0, 0, 0.3)");
+    }
+
+    // eslint-disable-next-line no-undef
+    setTimeout(() => {
+      setBorderColor("transparent");
+    }, 500);
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { borderColor }]}>
       <StatusBar style="auto" />
       <Header />
-      {children}
+      <Bottle flashBorderColor={flashBorderColor} />
     </View>
   );
 };
@@ -19,5 +35,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#e6ebe0",
     alignItems: "center",
     justifyContent: "center",
+    borderWidth: 10,
+    borderColor: "transparent",
   },
 });
