3320b1069ff47765c32bb8b0b823cfc7199fb4dd
cnievaslozano <cnievaslozano@gmail.com>
2024-07-19 19:48:46 +0200

    remove bottle

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import { Layout } from "./src/components/Layout";
-import { Bottle } from "./src/components/Bottle";
 import { loadConfig } from "./src/config";
 
 export default function App() {
   loadConfig(); // asyncStorage load for configuration
 
-  return (
-    <Layout>
-      <Bottle />
-    </Layout>
-  );
+  return <Layout />;
 }
