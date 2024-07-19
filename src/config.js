49aaff0ed7712747ed1e98be2d937b2690483284
cnievaslozano <cnievaslozano@gmail.com>
2024-07-19 16:50:14 +0200

    check & update Streak

--- src/config.js
+++ src/config.js
@@ -6,8 +6,9 @@ import {
 let config = {
   dailyGoal: 2000, // default
   glassCapacity: 250, // default
-  streak: 2,
-  totalWater: 1000,
+  streak: 0,
+  totalWater: 0,
+  lastGoalDate: null, // date of the last day that the goal was done
 };
 
 export const loadConfig = async () => {
@@ -22,4 +23,31 @@ export const getConfig = () => config;
 export const updateConfig = async (newConfig) => {
   config = { ...config, ...newConfig };
   await storeConfigInStorage(config);
+};
+
+export const checkAndUpdateStreak = async () => {
+  const today = new Date().toDateString();
+
+  // If today's goal has already been done
+  if (config.lastGoalDate === today) return;
+
+  // If the water consumed today is enough to goal
+  if (config.totalWater >= config.dailyGoal) {
+    if (config.lastGoalDate) {
+      const lastDate = new Date(config.lastGoalDate);
+      const diffInTime = new Date(today).getTime() - lastDate.getTime();
+      const diffInDays = diffInTime / (1000 * 3600 * 24);
+
+      if (diffInDays === 1) {
+        config.streak += 1; // Increase the streak if the previous day also done the goal
+      } else {
+        config.streak = 0; // Reset streak if you have skipped a day
+      }
+    } else {
+      config.streak = 1; // Firts time goal done
+    }
+
+    config.lastGoalDate = today; // Update date
+    await updateConfig(config);
+  }
 };
